5aa296509a14b121f61fdd9e3987d332ad3b685f
Igor <igor.kh.mail@gmail.com>
2013-01-14 16:29:50 +0200

    playing with navigation controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,28 +4,27 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    # This is our new line!
-    #@redViewController  = RedController.alloc.initWithNibName(nil, bundle: nil)
-    #@blueViewController  = BlueController.alloc.initWithNibName(nil, bundle: nil)
-    #@greenViewController  = GreenController.alloc.initWithNibName(nil, bundle: nil)
-    #@navController = UINavigationController.alloc.initWithRootViewController(@greenViewController)
-    #@window.rootViewController = @navController
+    @redViewController  = RedController.alloc.initWithNibName(nil, bundle: nil)
+    @blueViewController  = BlueController.alloc.initWithNibName(nil, bundle: nil)
+    @greenViewController  = GreenController.alloc.initWithNibName(nil, bundle: nil)
+    @navController = UINavigationController.alloc.initWithRootViewController(@greenViewController)
+    @window.rootViewController = @navController
     
-    #@navController.pushViewController(@redViewController, animated:true) 
-   # @navController.pushViewController(@blueViewController, animated:true) 
+    #@navController.pushViewController(@redViewController, animated:false)
+    #@navController.pushViewController(@blueViewController, animated:false)
 
     
-      @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
-        @blue_view.backgroundColor = UIColor.blueColor
-        @window.addSubview(@blue_view)
+      #@blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+      #  @blue_view.backgroundColor = UIColor.blueColor
+      #  @window.addSubview(@blue_view)
+      #
+      #  @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+      #  @green_view.backgroundColor = UIColor.greenColor
+      #  @window.addSubview(@green_view)
+      #
+      # @red_view  = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+      #  @red_view.backgroundColor = UIColor.redColor
+      # @blue_view.addSubview(@red_view)
 
-        @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-        @green_view.backgroundColor = UIColor.greenColor
-        @window.addSubview(@green_view)
-
-       @red_view  = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-        @red_view.backgroundColor = UIColor.redColor
-       @blue_view.addSubview(@red_view)
-    true
   end
 end
--- app/controllers/GreenController.rb
+++ app/controllers/GreenController.rb
@@ -3,5 +3,38 @@ class GreenController < UIViewController
     super
 
     self.view.backgroundColor = UIColor.greenColor
+
+    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @button.setFrame([[100, 100], [400, 100]])
+    @button.setTitle('Push me Red!', forState:UIControlStateNormal)
+    @button.addTarget(self, action:'pushRed', forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(@button)
+
+
+    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @button.setFrame([[100, 300], [400, 100]])
+    @button.setTitle('Push me Blue!', forState:UIControlStateNormal)
+    @button.addTarget(self, action:'pushBlue', forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(@button)
+
   end
+
+  def pushRed
+
+
+    @redViewController  = RedController.alloc.initWithNibName(nil, bundle: nil)
+    @navController = self.navigationController
+    @navController.pushViewController(@redViewController, animated:true)
+
+  end
+
+  def pushBlue
+
+
+    @blueViewController  = BlueController.alloc.initWithNibName(nil, bundle: nil)
+    @navController = self.navigationController
+    @navController.pushViewController(@blueViewController, animated:true)
+
+  end
+
 end
